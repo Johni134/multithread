5224cd3309d58811dafca0f6546b77958054962d
Johni134 <kec@inbox.ru>
2018-10-30 23:28:46 +0700

    lesson 5 done

--- src/main/java/ru/geekbrains/MainClass.java
+++ src/main/java/ru/geekbrains/MainClass.java
@@ -5,24 +5,45 @@ import ru.geekbrains.model.Race;
 import ru.geekbrains.model.Road;
 import ru.geekbrains.model.Tunnel;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 /**
  * Hello world!
  *
  */
 public class MainClass
 {
-    public static final int CARS_COUNT = 4;
+    private static final int CARS_COUNT = 4;
     public static void main(String[] args) {
+        final CyclicBarrier cyclicBarrier = new CyclicBarrier(CARS_COUNT);
+        final CountDownLatch cdl = new CountDownLatch(CARS_COUNT);
+        final CountDownLatch cdlLast = new CountDownLatch(CARS_COUNT);
+        final Semaphore semaphore = new Semaphore(CARS_COUNT / 2);
+        final AtomicBoolean winnerDefined = new AtomicBoolean(false);
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
-        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Race race = new Race(new Road(60), new Tunnel(semaphore), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), cyclicBarrier, cdl, cdlLast, winnerDefined);
         }
-        for (int i = 0; i < cars.length; i++) {
-            new Thread(cars[i]).start();
+        for (Car car : cars) {
+            new Thread(car).start();
+        }
+        try {
+            cdl.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        cyclicBarrier.reset();
+        try {
+            cdlLast.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
--- src/main/java/ru/geekbrains/model/Car.java
+++ src/main/java/ru/geekbrains/model/Car.java
@@ -1,36 +1,60 @@
 package ru.geekbrains.model;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
+    final private AtomicBoolean winnerDefined;
+    final private CyclicBarrier cyclicBarrier;
+    final private CountDownLatch cdl;
+    final private CountDownLatch cdlLast;
     static {
         CARS_COUNT = 0;
     }
-    private Race race;
-    private int speed;
-    private String name;
-    public String getName() {
-        return name;
-    }
-    public int getSpeed() {
-        return speed;
-    }
-    public Car(Race race, int speed) {
+
+    final private Race race;
+    final private int speed;
+    final private String name;
+
+    public Car(Race race, int speed, CyclicBarrier cyclicBarrier, CountDownLatch cdl, CountDownLatch cdlLast, AtomicBoolean winnerDefined) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
+        this.cyclicBarrier = cyclicBarrier;
+        this.cdl = cdl;
+        this.cdlLast = cdlLast;
+        this.winnerDefined = winnerDefined;
     }
+
+    String getName() {
+        return name;
+    }
+
+    int getSpeed() {
+        return speed;
+    }
+
     @Override
     public void run() {
         try {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
+            cyclicBarrier.await();
+            cdl.countDown();
+            cyclicBarrier.await();
         } catch (Exception e) {
             e.printStackTrace();
         }
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
         }
+        if (!winnerDefined.getAndSet(true)) {
+            System.out.println(this.name + " - WIN");
+        }
+        cdlLast.countDown();
     }
 }
